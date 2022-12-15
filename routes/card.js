1779afd55d74a41291b10b8e3ece35f1852a959a
anikisana <younker12345@gmail.com>
2022-12-15 16:06:23 +0600

    cart display

--- routes/card.js
+++ routes/card.js
@@ -2,6 +2,18 @@ const { Router } = require("express");
 const router = Router();
 const Course = require('../models/course')
 
+function mapCartItems(cart) {
+    return cart.items.map(c=>({
+        ...c.courseId._doc, count:c.count
+    }));
+}
+
+function computePrice(courses) {
+    return courses.reduce((total, course)=>{
+        return total += course.price * course.count;
+    }, 0)
+}
+
 router.post('/add', async (req,res)=>{
     const course = await Course.findById(req.body.id);
     await req.user.addToCart(course);
@@ -9,12 +21,16 @@ router.post('/add', async (req,res)=>{
 })
 
 router.get('/', async (req,res) => {
-    const card = await Card.fetch();
+    const user = await req.user
+        .populate('cart.items.courseId');
+
+    const courses = mapCartItems(user.cart);
+
     res.render('card', {
         title: "Корзина",
         isCard: true,
-        courses:card.courses,
-        price: card.price
+        courses:courses,
+        price: computePrice(courses)
     })
 })
 
