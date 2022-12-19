28ff1344be78c224142bd32ba3aca94c0c7f3c40
anikisana <younker12345@gmail.com>
2022-12-19 16:53:47 +0600

    Saving a session

--- index.js
+++ index.js
@@ -27,16 +27,6 @@ const hbs = exphbs.create({
     defaultLayout: 'main', 
     extname: 'hbs',
     handlebars: allowInsecurePrototypeAccess(Handlebars)
-  });
-
-app.use(async (req,res,next)=>{
-    try {
-        const user = await User.findById('639a124da224e3efaf7b0af8');
-        req.user = user;
-        next();
-    } catch(e) {
-        console.log(e);
-    }
 });
 
 app.engine('hbs', hbs.engine);
@@ -71,15 +61,15 @@ async function start() {
         await mongoose.connect(url, {
             useNewUrlParser:true
         });
-        const candidate = await User.findOne();
-        if(!candidate) {
-            const newUser = new User({
-                email:"azamatryssalievyouz@gmail.com",
-                name:"Azamat",
-                cart: {items:[]}
-            })
-            await newUser.save();
-        }
+        // const candidate = await User.findOne();
+        // if(!candidate) {
+        //     const newUser = new User({
+        //         email:"azamatryssalievyouz@gmail.com",
+        //         name:"Azamat",
+        //         cart: {items:[]}
+        //     })
+        //     await newUser.save();
+        // }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         })
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const router = Router();
+const User = require('../models/user')
 
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
@@ -16,8 +17,17 @@ router.get('/logout', async (req, res) => {
     
 
 router.post('/login', async(req,res) => {
+    const user = await User.findById('639a124da224e3efaf7b0af8');
+    req.session.user = user;
     req.session.isAuthenticated = true;
-    res.redirect('/');
+
+    req.session.save(err => {
+        if(err) {
+            throw err;
+        } else {
+            res.redirect('/');
+        }
+    })
 })
 
 module.exports = router;
