6d279b4e3b74ade0cb72bbb0337b918129b7f0bc
anikisana <younker12345@gmail.com>
2022-12-23 22:19:59 +0600

    sendgrid

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGODB_URI : "mongodb+srv://doppelyouz:daneka18@cluster0.tt7avdq.mongodb.net/shop",
-    SESSION_SECRET : 'some secret value'
+    SESSION_SECRET : 'some secret value',
+    SENDGRID_API_KEY : "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"
 }
