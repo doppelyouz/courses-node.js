91c3ef0d42dabb322bf19a0d26ed98172e1068e8
anikisana <younker12345@gmail.com>
2022-12-24 20:30:52 +0600

    sending an email

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,7 @@
 module.exports = {
     MONGODB_URI : "mongodb+srv://doppelyouz:daneka18@cluster0.tt7avdq.mongodb.net/shop",
     SESSION_SECRET : 'some secret value',
-    SENDGRID_API_KEY : "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"
+    UNISENDER_API_KEY : "69hy557pwh7tdkrq6fsgr5fyqkjgk8hidn8hc5ko",
+    EMAIL_FROM : 'azamatryssalievyouz@gmail.com',
+    BASE_URL : 'http://localhost:3000'
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,17 @@
 const {Router} = require('express');
 const router = Router();
+
+const keys = require('../keys');
+const regEmail = require('../emails/registration');
+
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+
+const UniSender = require('unisender');   
+const uniSender = new UniSender({     
+    api_key: keys.UNISENDER_API_KEY,  
+    lang: 'ru'               
+});
 
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
@@ -64,6 +74,10 @@ router.post('/register', async (req,res) => {
 
             await user.save();
             res.redirect('/auth/login#login');
+            await uniSender.sendEmail(regEmail(email)).then(function (response) {
+                console.log('Message id: ' + response.result.message_id);
+            }).catch(function (response) {
+                console.log('Error:' + response.error);});;
         }
     } catch(e) {
         console.log(e);
